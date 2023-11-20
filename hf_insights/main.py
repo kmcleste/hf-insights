f120e32d7bdc5847a2d06bc1f8edab678a805573
Kyle McLester <kmcleste@uncc.edu>
2023-11-20 18:29:25 -0500

    small data

--- hf_insights/main.py
+++ hf_insights/main.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 @st.cache_data
 def create_df() -> pd.DataFrame:
-    filepath: pathlib.Path = pathlib.Path(os.getcwd(), "data", "models.csv")
+    filepath: pathlib.Path = pathlib.Path(os.getcwd(), "data", "small.csv")
     return pd.read_csv(filepath_or_buffer=filepath, sep=",")
 
 
